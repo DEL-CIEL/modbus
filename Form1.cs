7e65d4c1023646983ea6e3dd987ee4f15c853c63
Minimax Femto <charlemax10@gmail.com>
2025-10-09 09:18:41 +0200

    #3 connexion / deconnexion

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,32 @@ namespace modbus
         {
             if(textBoxAdresseIP.TextLength > 6)
             {
-                textBox1.AppendText("Connexion au serveur " + textBoxAdresseIP.Text + "\n");
+                string ipAdress = this.textBoxAdresseIP.Text;
+                this.textBox1.AppendText("Connexion au serveur " + ipAdress + "\n");
+                this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint iped = new IPEndPoint(IPAddress.Parse(ipAdress), 502);
+                try
+                {
+                    this.socket.Connect(iped);
+                    this.textBox1.AppendText("Connexion ok\n");
+                }
+                catch (System.Exception ex)
+                {
+                    this.textBox1.AppendText("**Exception : Impossible de se connecter au serveur : " + ex.Message + "\r\n");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.socket.Close();
+                this.textBox1.AppendText("Deconnexion reussie\n");
+            }
+            catch (System.Exception ex)
+            {
+                this.textBox1.AppendText("Deconnexion impossible: " + ex.Message + "\n");
             }
         }
     }
