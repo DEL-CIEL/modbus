52a689ff4d8c7c1f555457d1f6de0f93cd889062
Minimax Femto <charlemax10@gmail.com>
2025-10-09 08:51:23 +0200

    #2 affichage de la connexion

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAdresseIP = new System.Windows.Forms.Label();
+            this.textBoxAdresseIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelAdresseIP
+            // 
+            this.labelAdresseIP.AccessibleName = "";
+            this.labelAdresseIP.AutoSize = true;
+            this.labelAdresseIP.Location = new System.Drawing.Point(21, 31);
+            this.labelAdresseIP.Name = "labelAdresseIP";
+            this.labelAdresseIP.Size = new System.Drawing.Size(57, 13);
+            this.labelAdresseIP.TabIndex = 0;
+            this.labelAdresseIP.Text = "Ip serveur:";
+            // 
+            // textBoxAdresseIP
+            // 
+            this.textBoxAdresseIP.Location = new System.Drawing.Point(84, 28);
+            this.textBoxAdresseIP.Name = "textBoxAdresseIP";
+            this.textBoxAdresseIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdresseIP.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connexion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Deconnexion";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(411, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 300);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 338);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxAdresseIP);
+            this.Controls.Add(this.labelAdresseIP);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAdresseIP;
+        private System.Windows.Forms.TextBox textBoxAdresseIP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,19 @@ namespace modbus
 {
     public partial class Form1 : Form
     {
+        private Socket socket;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBoxAdresseIP.TextLength > 6)
+            {
+                textBox1.AppendText("Connexion au serveur " + textBoxAdresseIP.Text + "\n");
+            }
         }
     }
 }
