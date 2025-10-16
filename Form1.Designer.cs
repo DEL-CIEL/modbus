a66571ae195c807d2484134f8b80366e137a1a35
Minimax Femto <charlemax10@gmail.com>
2025-10-16 11:04:49 +0200

    #4 lecture tension

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelAdresseIP
@@ -80,18 +83,46 @@
             this.textBox1.Size = new System.Drawing.Size(213, 300);
             this.textBox1.TabIndex = 4;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Lire tension";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(301, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(79, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tension moteur en Volt";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(636, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxAdresseIP);
             this.Controls.Add(this.labelAdresseIP);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +135,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace modbus
 {
@@ -34,7 +35,7 @@ namespace modbus
                     this.socket.Connect(iped);
                     this.textBox1.AppendText("Connexion ok\n");
                 }
-                catch (System.Exception ex)
+                catch (Exception ex)
                 {
                     this.textBox1.AppendText("**Exception : Impossible de se connecter au serveur : " + ex.Message + "\r\n");
                 }
@@ -48,10 +49,30 @@ namespace modbus
                 this.socket.Close();
                 this.textBox1.AppendText("Deconnexion reussie\n");
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 this.textBox1.AppendText("Deconnexion impossible: " + ex.Message + "\n");
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var trameE = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x01, 0x03, 0x0C, 0x86, 0x00, 0x01 };
+            try
+            {
+                this.socket.Send(trameE);
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.AppendText("Envoi impossible: " + ex.Message + "\n");
+            }
+
+            var trameR = new byte[256];
+
+            int bytesReceived = this.socket.Receive(trameR);
+            int tensionRaw = (trameR[9] << 8) | trameR[10];
+            double tension = tensionRaw / 10.0;
+            textBox2.Text = tension.ToString();
         }
     }
 }
